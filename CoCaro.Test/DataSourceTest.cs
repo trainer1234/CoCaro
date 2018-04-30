683da1c961b6838163ccc0cfc8d59c276c8a557e
Kha Giai Minh <trainer1234.giaiminh@gmail.com>
2018-04-30 18:18:14 +0700

    parameterized test

--- CoCaro.Test/DataSourceTest.cs
+++ CoCaro.Test/DataSourceTest.cs
@@ -27,10 +27,12 @@ namespace CoCaro.Test
             mockDataSource = new Mock<IDataSource>();
         }
 
-        [Test]
-        public void GetGameHistory_ReturnEmpty()
+        //[Test]
+        [TestCaseSource(typeof(TestData), "GetEmptyMockHistoryData")]
+        public void GetGameHistory_ReturnEmpty(List<Game> games)
         {
-            var data = new List<Game>().AsQueryable();
+            var data = games.AsQueryable();
+            //var data = new List<Game>().AsQueryable();
             // Arrange
             var mockSet = new Mock<DbSet<Game>>();
 
@@ -51,74 +53,10 @@ namespace CoCaro.Test
             Assert.That(historyData, Is.Empty);
         }
 
-        //[TestCaseSource(typeof(TestData), "GetMockHistoryData")]
-        [Test]
-        public void GetGameHistory_Always_ReturnValues()
+        [TestCaseSource(typeof(TestData), "GetMockHistoryData")]
+        public void GetGameHistory_Always_ReturnValues(List<Game> games)
         {
-            var moveData1 = new List<Move>()
-            {
-                new Move { Id = 1, GameId = 1, Point = "A1" },
-                new Move { Id = 2, GameId = 1, Point = "B1" },
-                new Move { Id = 3, GameId = 1, Point = "A2" },
-                new Move { Id = 4, GameId = 1, Point = "B2" },
-                new Move { Id = 5, GameId = 1, Point = "A3" },
-                new Move { Id = 6, GameId = 1, Point = "B3" },
-                new Move { Id = 7, GameId = 1, Point = "A4" },
-                new Move { Id = 8, GameId = 1, Point = "B4" },
-                new Move { Id = 9, GameId = 1, Point = "A5" }
-            };
-            var moveData2 = new List<Move>()
-            {
-                new Move { Id = 10, GameId = 2, Point = "A1" },
-                new Move { Id = 11, GameId = 2, Point = "B1" },
-                new Move { Id = 12, GameId = 2, Point = "A2" },
-                new Move { Id = 13, GameId = 2, Point = "B2" },
-                new Move { Id = 14, GameId = 2, Point = "A3" },
-                new Move { Id = 15, GameId = 2, Point = "B3" },
-                new Move { Id = 16, GameId = 2, Point = "A4" },
-                new Move { Id = 17, GameId = 2, Point = "B4" },
-                new Move { Id = 18, GameId = 2, Point = "A5" }
-            };
-            var moveData3 = new List<Move>()
-            {
-                new Move { Id = 19, GameId = 3, Point = "A1" },
-                new Move { Id = 20, GameId = 3, Point = "B1" },
-                new Move { Id = 21, GameId = 3, Point = "A2" },
-                new Move { Id = 22, GameId = 3, Point = "B2" },
-                new Move { Id = 23, GameId = 3, Point = "A3" },
-                new Move { Id = 24, GameId = 3, Point = "B3" },
-                new Move { Id = 25, GameId = 3, Point = "A4" },
-                new Move { Id = 26, GameId = 3, Point = "B4" },
-                new Move { Id = 27, GameId = 3, Point = "A5" }
-            };
-
-            var data = new List<Game>()
-            {
-                new Game
-                {
-                    Id = 1,
-                    Winner = 1,
-                    StartTime = DateTime.Now,
-                    Duration = 12,
-                    Moves = moveData1
-                },
-                new Game
-                {
-                    Id = 2,
-                    Winner = 1,
-                    StartTime = DateTime.Now,
-                    Duration = 12,
-                    Moves = moveData2
-                },
-                new Game
-                {
-                    Id = 3,
-                    Winner = 1,
-                    StartTime = DateTime.Now,
-                    Duration = 12,
-                    Moves = moveData3
-                }
-            }.AsQueryable();
+            var data = games.AsQueryable();
             
             // Arrange
             var mockSet = new Mock<DbSet<Game>>();
@@ -130,7 +68,7 @@ namespace CoCaro.Test
 
             mockSet.Setup(m => m.Include("Moves")).Returns(mockSet.Object);
             mockCaroContext.Setup(c => c.Games).Returns(mockSet.Object);
-
+            
             dataSource = new DataSource(mockCaroContext.Object);
             var expectedResult = data.ToList();
 
@@ -141,7 +79,7 @@ namespace CoCaro.Test
             mockDataSource.VerifyAll();
 
             Assert.That(historyData, !Is.Empty);
-            Assert.AreEqual(3, historyData.Count);
+            Assert.AreEqual(games.Count, historyData.Count);
             for (int i = 0; i < expectedResult.Count; i++)
             {
                 Assert.AreEqual(expectedResult[i].Id, historyData[i].Id);
--- CoCaro.Test/TestData.cs
+++ CoCaro.Test/TestData.cs
@@ -1,4 +1,5 @@
-﻿using CoCaro.Model;
+﻿using CoCaro.DAL.Models;
+using CoCaro.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,51 +10,148 @@ namespace CoCaro.Test
 {
     public class TestData
     {
-        public static IEnumerable<IEnumerable<ChessBoard>> GetMockHistoryData
+        public static IEnumerable<IEnumerable<Game>> GetMockHistoryData
         {
             get
             {
-                yield return new List<ChessBoard>()
+                var moveData1 = new List<Move>()
                 {
-                    new ChessBoard
+                    new Move { Id = 1, GameId = 1, Point = "A1" },
+                    new Move { Id = 2, GameId = 1, Point = "B1" },
+                    new Move { Id = 3, GameId = 1, Point = "A2" },
+                    new Move { Id = 4, GameId = 1, Point = "B2" },
+                    new Move { Id = 5, GameId = 1, Point = "A3" },
+                    new Move { Id = 6, GameId = 1, Point = "B3" },
+                    new Move { Id = 7, GameId = 1, Point = "A4" },
+                    new Move { Id = 8, GameId = 1, Point = "B4" },
+                    new Move { Id = 9, GameId = 1, Point = "A5" }
+                };
+                var moveData2 = new List<Move>()
+                {
+                    new Move { Id = 10, GameId = 2, Point = "A1" },
+                    new Move { Id = 11, GameId = 2, Point = "B1" },
+                    new Move { Id = 12, GameId = 2, Point = "A2" },
+                    new Move { Id = 13, GameId = 2, Point = "B2" },
+                    new Move { Id = 14, GameId = 2, Point = "A3" },
+                    new Move { Id = 15, GameId = 2, Point = "B3" },
+                    new Move { Id = 16, GameId = 2, Point = "A4" },
+                    new Move { Id = 17, GameId = 2, Point = "B4" },
+                    new Move { Id = 18, GameId = 2, Point = "A5" }
+                };
+                var moveData3 = new List<Move>()
+                {
+                    new Move { Id = 19, GameId = 3, Point = "A1" },
+                    new Move { Id = 20, GameId = 3, Point = "B1" },
+                    new Move { Id = 21, GameId = 3, Point = "A2" },
+                    new Move { Id = 22, GameId = 3, Point = "B2" },
+                    new Move { Id = 23, GameId = 3, Point = "A3" },
+                    new Move { Id = 24, GameId = 3, Point = "B3" },
+                    new Move { Id = 25, GameId = 3, Point = "A4" },
+                    new Move { Id = 26, GameId = 3, Point = "B4" },
+                    new Move { Id = 27, GameId = 3, Point = "A5" }
+                };
+
+                yield return new List<Game>
+                {
+                    new Game
                     {
                         Id = 1,
                         Winner = 1,
-                        NumberOfMove = 12,
-                        IsEnd = true,
-                        GameDuration = 50,
                         StartTime = DateTime.Now,
-                        Moves = new List<string> { "A1", "B1", "A2", "B2", "A3", "B3", "A4", "B4", "A5" }
+                        Duration = 12,
+                        Moves = moveData1
                     },
-                    new ChessBoard
+                    new Game
                     {
                         Id = 2,
                         Winner = 1,
-                        NumberOfMove = 12,
-                        IsEnd = true,
-                        GameDuration = 50,
                         StartTime = DateTime.Now,
-                        Moves = new List<string> { "A1", "B1", "A2", "B2", "A3", "B3", "A4", "B4", "A5" }
+                        Duration = 12,
+                        Moves = moveData2
                     },
-                    new ChessBoard
+                    new Game
                     {
                         Id = 3,
                         Winner = 1,
-                        NumberOfMove = 12,
-                        IsEnd = true,
-                        GameDuration = 50,
                         StartTime = DateTime.Now,
-                        Moves = new List<string> { "A1", "B1", "A2", "B2", "A3", "B3", "A4", "B4", "A5" }
+                        Duration = 12,
+                        Moves = moveData3
                     }
                 };
+
+                yield return new List<Game>
+                {
+                    new Game
+                    {
+                        Id = 1,
+                        Winner = 1,
+                        StartTime = DateTime.Now,
+                        Duration = 12,
+                        Moves = moveData1
+                    },
+                    new Game
+                    {
+                        Id = 2,
+                        Winner = 1,
+                        StartTime = DateTime.Now,
+                        Duration = 12,
+                        Moves = moveData2
+                    }
+                };
+
+                yield return new List<Game>
+                {
+                    new Game
+                    {
+                        Id = 1,
+                        Winner = 1,
+                        StartTime = DateTime.Now,
+                        Duration = 12,
+                        Moves = moveData1
+                    }
+                };
+
+                //yield return new List<ChessBoard>()
+                //{
+                //    new ChessBoard
+                //    {
+                //        Id = 1,
+                //        Winner = 1,
+                //        NumberOfMove = 12,
+                //        IsEnd = true,
+                //        GameDuration = 50,
+                //        StartTime = DateTime.Now,
+                //        Moves = new List<string> { "A1", "B1", "A2", "B2", "A3", "B3", "A4", "B4", "A5" }
+                //    },
+                //    new ChessBoard
+                //    {
+                //        Id = 2,
+                //        Winner = 1,
+                //        NumberOfMove = 12,
+                //        IsEnd = true,
+                //        GameDuration = 50,
+                //        StartTime = DateTime.Now,
+                //        Moves = new List<string> { "A1", "B1", "A2", "B2", "A3", "B3", "A4", "B4", "A5" }
+                //    },
+                //    new ChessBoard
+                //    {
+                //        Id = 3,
+                //        Winner = 1,
+                //        NumberOfMove = 12,
+                //        IsEnd = true,
+                //        GameDuration = 50,
+                //        StartTime = DateTime.Now,
+                //        Moves = new List<string> { "A1", "B1", "A2", "B2", "A3", "B3", "A4", "B4", "A5" }
+                //    }
+                //};
             }
         }
 
-        public static IEnumerable<ChessBoard> GetEmptyMockHistoryData
+        public static IEnumerable<IEnumerable<Game>> GetEmptyMockHistoryData
         {
             get
             {
-                return new List<ChessBoard>();
+                yield return new List<Game>();
             }
         }
 
