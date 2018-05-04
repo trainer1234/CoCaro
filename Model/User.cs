e980e96a384502451ebffa73b1c4104843a5a9bf
DESKTOP-TU3D1ST\SONY <kd230997@gmail.com>
2018-05-04 21:30:07 +0700

    Fix Conflict

--- Model/User.cs
+++ Model/User.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace CoCaro.Model
 {
-    class User
+    public class User
     {
+        private int iDUsers;
+        private string name;
+        private string tittle;
+
+        public string Tittle { get => tittle; set => tittle = value; }
+        public string Name { get => name; set => name = value; }
+        public int IDUsers { get => iDUsers; set => iDUsers = value; }
     }
 }
